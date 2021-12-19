41caa459f003012319b79605a836cfe8edcffb7b
Piorosen <aoikazto@gmail.com>
2021-12-19 19:58:29 +0900

    support spm

---
+++ Tests/BMPlayerTests/BMPlayerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BMPlayer
+
+final class BMPlayerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BMPlayer().text, "Hello, World!")
+    }
+}
