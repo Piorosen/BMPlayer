3a295fd997fb90024571689da013af0cbb605f3f
Piorosen <aoikazto@gmail.com>
2021-12-19 20:15:11 +0900

    캐시 삭제

--- Tests/BMPlayerTests/BMPlayerTests.swift
+++ Tests/BMPlayerTests/BMPlayerTests.swift
@@ -6,6 +6,6 @@ final class BMPlayerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(BMPlayer().text, "Hello, World!")
+//        XCTAssertEqual(BMPlayer().text, "Hello, World!")
     }
 }
